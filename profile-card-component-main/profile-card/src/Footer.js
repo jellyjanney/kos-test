4b479b650caf309bd2d71f29d98bd9d68d42e454
darunee_p <darunee_p@zmyhome.com>
2023-09-11 15:07:43 +0700

    profile card

---
+++ profile-card-component-main/profile-card/src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+        return (
+                <footer class="attribution">
+                        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+                        Coded by <a href="#">jellyjanney</a>.
+                </footer>
+        );
+}
+
+export default Footer;
